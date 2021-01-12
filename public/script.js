4c154e32d9a56614a8694b067f2a5c9f44fee76a
Eric Liu <ericliu314@gmail.com>
2021-01-11 19:56:58 -0500

    Add model

--- public/script.js
+++ public/script.js
@@ -1,5 +1,46 @@
 class Model {
-  constructor() {}
+  #bookmarks;
+  #searchResults;
+  #article;
+
+  constructor() {
+    // { id, source, author, title, description, url, urlToImage, publishedAt }
+    this.#bookmarks = localStorage.getItem(LOCAL_STORAGE_KEY)
+      ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      : [];
+    this.#searchResults = [];
+    this.#article = null;
+  }
+
+  addBookmark(article) {
+    this.#bookmarks.push(article);
+  }
+
+  deleteBookmark(id) {
+    const index = this.#bookmarks.find(article => article.id === id);
+    if (index === -1) {
+      return;
+    }
+    this.#bookmarks.splice(index, 1);
+  }
+
+  save() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.#bookmarks));
+  }
+
+  #updateSearchResults(searchResults) {
+    this.#searchResults = searchResults;
+  }
+
+  updateArticle(article) {
+    this.#article = article;
+  }
+
+  async search(query) {
+    const response = await fetch(`/news?q=${query}`);
+    const data = await response.json();
+    this.#updateSearchResults(data)
+  }
 }
 
 class View {
@@ -20,5 +61,7 @@ class Controller {
     this.#view = view;
   }
 }
+
+const LOCAL_STORAGE_KEY = 'NewsAggregatorProject';
 
 const app = new Controller(new Model(), new View());
